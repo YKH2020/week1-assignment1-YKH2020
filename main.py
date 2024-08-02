2311192167044f916060264d65e8c5fdb2b1a679
YKH2020 <73809628+YKH2020@users.noreply.github.com>
2024-08-02 17:19:59 +0000

    main.py changes complete

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ To pass, all automated testing of your inputs have to pass their tests.
 # * The exception_message is correctly defined as a string
 def create_message():
     try:
-        exception_message = "This message is broken " + 1
+        exception_message = "This message is broken " + str(1)
     except RuntimeError:
         raise
     return exception_message
@@ -21,18 +21,18 @@ def create_message():
 # create a single string. The single_string should be a string and not a `None`
 name = "Monday"
 greeting = "Hello students!"
-single_string = None
+single_string = f"{greeting} It's {name}"
 
 
 # Use the following text to create a string and assign it to quoted_string:
 # Sometimes you can use a ' character to denote minutes or " for seconds
-quoted_string = None
+quoted_string = """Sometimes you can use a ' character to denote minutes or " for seconds"""
 
 
 # The current condition does not evaluate correctly. Python has truthy values
 # and dictionaries is one of them. Update the code so that has_contacts will
 # evaluate to True instead of False
-contacts = {}
+contacts = {"Key": "Value"}
 if contacts:
     has_contacts = True
 else:
@@ -45,7 +45,7 @@ else:
 # `apples` and `cost` variables
 apples = 4
 cost = 7.3
-numbers_string = None
+numbers_string = f"I had {apples} apples which cost {cost} in total."
 
 
 # from the following dictionary, define the file_sizes variable to hold all the sizes
@@ -57,11 +57,11 @@ files = {
     "objects": 55,
     "packed-refs refs": 120,
 }
-file_sizes = None
+file_sizes = files.values
 
 # from the same `files` dictionary, define the file_names variable so that it holds
 # the names only
-file_names = None
+file_names = files.keys
 
 
 # from the list of first names below write code that gets rid of Melvin and Carlos
@@ -79,7 +79,8 @@ first_names = [
     'Michael'
  ]
 
-new_first_names = None
+new_first_names = first_names.pop(1)
+new_first_names = first_names.pop(-2)
 
 # from the same list of first names, assign the last 4 names to the last_four_names
 first_names = [
@@ -94,7 +95,7 @@ first_names = [
     'Carlos',
     'Michael'
  ]
-last_four_names = None
+last_four_names = first_names[-4:]
 
 
 # The following code breaks the function. You haven't seen
@@ -103,4 +104,4 @@ last_four_names = None
 # * a fallback value of "John Doe" is returned
 def get_name():
     information = {}
-    return information["name"]
+    return information.get("Default Value" ,"John Doe")
